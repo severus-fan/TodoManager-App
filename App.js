aeaed6d475b060957587df3397941c5b61f7b771
severus-fan <severus_fan@outlook.com>
2021-08-12 15:37:43 +0800

    add

--- App.js
+++ App.js
@@ -1,145 +1,37 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, Button, StyleSheet, View, Text, TextInput, TouchableOpacity } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { BlurView } from 'expo-blur';
-import { getHeaderTitle } from '@react-navigation/elements';
-import {TodoItem} from './components/TodoItem';
+import React, { useState } from "react";
+import { Button, Text, View } from "react-native";
 
+const Cat = (props) => {
+  const [isHungry, setIsHungry] = useState(true);
 
-function CalendarScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-            <Button
-                title="Go to Settings"
-                onPress={() => navigation.navigate('Settings')}
-            />
-        </View>
-    );
-}
-function TodoScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-            <FlatList
-                // data={todoList}
-                // keyExtractor={(item, index) => item.id.toString()}
-                renderItem={({ item, index }) => <TodoItem dataItem={item} key={item.id} checkTodo={(item) => this._checkTodo(item)} />}
-            />
-            <TouchableOpacity style={styles.addBtn} onPress={() => { alert("add") }}>
-                <Ionicons name="md-add" size={38} color="#fff" />
-            </TouchableOpacity>
-        </View>
-    );
+  return (
+    <View>
+      <Text>
+        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
+      </Text>
+      <Button
+        onPress={() => {
+          setIsHungry(false);
+        }}
+        disabled={!isHungry}
+        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+      />
+    </View>
+  );
 }
 
-function SettingsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Settings!</Text>
-            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-        </View>
-    );
+function App() {
+  // 声明一个叫 “count” 的 state 变量。
+  const [count, setCount] = useState(0);
+
+  return (
+    <View>
+      <Text>You clicked {count} times</Text>
+      <Button onClick={() => setCount(count + 1)}
+      title="aaa">
+      </Button>
+    </View>
+  );
 }
 
-const Tab = createBottomTabNavigator();
-
-export default function App() {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    // tabBarShowLabel:false,
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-                        switch (route.name) {
-                            case "Calendar":
-                                iconName = focused ? 'calendar' : 'calendar-outline';
-                                break;
-                            case "Todo":
-                                iconName = focused ? 'list' : 'list-outline';
-                                break;
-                            case "Settings":
-                                iconName = focused ? 'settings' : 'settings-outline';
-                                break;
-                        }
-                        return <Ionicons name={iconName} size={30} color={color} />;
-                    },
-                    tabBarActiveTintColor: 'tomato',
-                    tabBarInactiveTintColor: 'gray',
-                })}
-            >
-                <Tab.Screen name="Calendar" component={CalendarScreen} />
-                <Tab.Screen name="Todo" component={TodoScreen} />
-                <Tab.Screen name="Settings" component={SettingsScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-}
-
-const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-        backgroundColor: "#fff",
-        padding: 10,
-    },
-    addBtn: {
-        position: "relative",
-        left: 130,
-        bottom: 20,
-        width: 50,
-        height: 50,
-        borderRadius: 20,
-        backgroundColor: "tomato",
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'center'
-    }
-})
-
-
-
-
-// import CalendarScreen from './screens/calendar'
-// import TodoScreen from './screens/todo'
-// import SettingsScreen from './screens/settings'
-
-// export const TodolistTabs = createBottomTabNavigator({
-//     Calendar: { screen: CalendarScreen },
-//     Todo: { screen: TodoScreen },
-//     Settings: { screen: SettingsScreen },
-// },
-//     {
-//         navigationOptions: ({ navigation }) => ({
-//             tabBarIcon: ({ focused, tintColor }) => {
-//                 const { routeName } = navigation.state;
-//                 let iconName;
-//                 switch (routeName) {
-//                     case "Calendar":
-//                         iconName = focused ? 'calendar' : 'calendar-outline';
-//                         break;
-//                     case "Todo":
-//                         iconName = focused ? 'list' : 'list-outline';
-//                         break;
-//                     case "Settings":
-//                         iconName = focused ? 'settings' : 'settings-outline';
-//                         break;
-//                 }
-//                 return <Ionicons name={iconName} size={25} color={tintColor} />
-//             }
-//         }),
-//         tabBarOptions: {
-//             activeTintColor: '#FF4081',
-//             inactiveTintColor: '#455A64',
-//             labelStyle: {
-//                 fontSize: 13,
-//             },
-//             style: {
-//                 backgroundColor: '#fff',
-//             }
-//         },
-//     })
+export default App;
